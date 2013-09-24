21b261d7e01278ee88adbd55283f851b55bd1461
Sam Beveridge <sbeveridge@insidesales.com>
2013-09-24 09:22:24 -0600

    added the weather man

---
+++ apis/weatherman.py
@@ -0,0 +1,26 @@
+def currentWeather(city, state):
+    """
+    get the current weather for the given city, state
+    @param city: String, the city name
+    @param state: String, 2 letter state abbreviation (UT)
+    @return weather: Dict with status, temp rain (mm), and cloud %
+    """
+    import requests
+    import json
+
+    # send the request and get the data
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Provo,Ut')
+    data = json.loads(r.text)
+    weather = {
+        'status': '',
+        'temp': '',
+        'rain': '',
+        'clouds': ''
+    }
+
+    weather['status'] = data['weather'][0]['main']
+    weather['temp'] = int((data['main']['temp'] - 273.15)*1.8000 + 32) # convert Kelvin to F
+    weather['rain'] = '%smm' % data['rain']['3h']
+    weather['clouds'] = '%s percent' % data['clouds']['all']
+
+    return weather
